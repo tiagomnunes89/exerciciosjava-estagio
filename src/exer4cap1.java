0a42813879cd4ce5811733230a747b085be8442f
Tiago M. Nunes <tiagomnunes@hotmail.com>
2018-08-21 12:14:45 -0300

    Firt chapter exercises

---
+++ src/exer4cap1.java
@@ -0,0 +1,8 @@
+public class exer4cap1 {
+
+        public static void main (String args[]) {
+
+            //O comando abaixo é responsável pela impressão da informação na tela
+            System.out.println("Terminei o módulo 3 com um programa java.");
+        }
+}
---
+++ src/exer7cap1.java
@@ -0,0 +1,8 @@
+public class exer7cap1 {
+
+        public static void start(String args[]) {
+
+            //O comando abaixo é responsável pela impressão da informação na tela
+            System.out.println("Terminei o módulo 3 com um programa java.");
+        }
+}
